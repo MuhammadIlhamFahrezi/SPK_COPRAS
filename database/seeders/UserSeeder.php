c0a4a63846819630a0e73762fdcb83955488b7b2
ilham <ezirasyid@gmail.com>
2025-05-19 20:36:07 +0700

    Menimpa Project

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,10 +15,30 @@ class UserSeeder extends Seeder
     {
         // Membuat user admin
         DB::table('users')->insert([
-            'nama_lengkap' => 'Administrator',
-            'username' => 'admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('password'),
+            'nama_lengkap' => 'Ilham Fahrezi',
+            'username' => 'Ilham',
+            'email' => 'ilham@gmail.com',
+            'password' => Hash::make('ilhamCop'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'nama_lengkap' => 'Rayner Aditya',
+            'username' => 'Rayner',
+            'email' => 'rayner@gmail.com',
+            'password' => Hash::make('raynerCop'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        DB::table('users')->insert([
+            'nama_lengkap' => 'Anjuan Kaisar',
+            'username' => 'Anjuan',
+            'email' => 'anjuan@gmail.com',
+            'password' => Hash::make('anjuanCop'),
             'role' => 'admin',
             'created_at' => now(),
             'updated_at' => now(),
